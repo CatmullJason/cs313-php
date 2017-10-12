dc685ff48a98ec83fe9a76157ef8ba0afce25526
CatmullJason <jasoncatmull@gmail.com>
2017-10-11 18:01:39 -0600

    PHPTEST.php connection fixed?

--- web/PHPTEST.php
+++ web/PHPTEST.php
@@ -1,36 +1,48 @@
-<?php
-
-require "dbConnect.php";
-$db = get_db();
-
-?>
-
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Test Notes</title>
-</head>
-
 <body>
-<div>
-
-<h1>Notes</h1>
 
 <?php
 
-$statement = $db->prepare("SELECT username, password FROM note_user");
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:Arsenal1!@localhost:5432/tickets";
+}
+
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+$statement = $db->prepare("SELECT restaurant_id, user_id, date_created, max_priority, comment FROM public.ticket");
 $statement->execute();
 
-while ($row = $statment->fetch(PDO::FETCH_ASSOC))
-	{
-		echo $row['username'] . ' ' . $row['password'];
-	}
-
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	echo '<p>';
+	echo '<strong>' . $row['restaurant_id'] . ' ' . $row['user_id'] . ':';
+	echo $row['date_created'] . '</strong>' . ' - ' . $row['comment'];
+	echo '</p>';
+}
 
 ?>
-
-
-</div>
 
 </body>
 </html>
